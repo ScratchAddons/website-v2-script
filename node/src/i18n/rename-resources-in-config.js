e5cfbd4f9d792bb4b45757f812fb0de5642bef61
Hans5958 <email5958.1@gmail.com>
2021-05-19 21:43:26 +0700

    Fix errors

--- node/src/i18n/rename-resources-in-config.js
+++ node/src/i18n/rename-resources-in-config.js
@@ -1,7 +1,7 @@
 const fs = require("fs-extra")
 
 module.exports = configFilePath => {
-	const configFile = fs.readFileSync(configFilePath, "utf-8")
+	let configFile = fs.readFileSync(configFilePath, "utf-8")
 
 	configFile = configFile.replace(/\[scratch-addons-website.en_(.+)\]/g, (match, one) => {
 		return `[scractch-addons-website.${one.replace("\\", "/")}]`
--- sh/i18n-update-source.sh
+++ sh/i18n-update-source.sh
@@ -1,5 +1,5 @@
 npm install --prefix ../script
 node ../script/node/ci/i18n-update-source.js
-tx config mapping-bulk --project scratch-addons-website --source-language en --type GITHUBMARKDOWN --file-extension .md --source-file-dir en/markdown --expression ^<lang^>/markdown/{filepath}/{filename}{extension} --execute
-tx config mapping-bulk --project scratch-addons-website --source-language en --type HTML --file-extension .html --source-file-dir en/html-content --expression ^<lang^>/html-content/{filepath}/{filename}{extension} --execute
+tx config mapping-bulk --project scratch-addons-website --source-language en --type GITHUBMARKDOWN --file-extension .md --source-file-dir en/markdown --expression <lang>/markdown/{filepath}/{filename}{extension} --execute
+tx config mapping-bulk --project scratch-addons-website --source-language en --type HTML --file-extension .html --source-file-dir en/html-content --expression <lang>/html-content/{filepath}/{filename}{extension} --execute
 node ../script/node/ci/i18n-update-source-2.js
