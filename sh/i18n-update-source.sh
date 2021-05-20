2f1916d65080a4367d9ac44818f13f83d1c760a0
Hans5958 <email5958.1@gmail.com>
2021-05-20 10:38:55 +0700

    Use HTML_FRAGMENT instead of HTML

--- sh/i18n-update-source.sh
+++ sh/i18n-update-source.sh
@@ -2,5 +2,5 @@ npm install --prefix ../script
 pip install transifex-client
 node ../script/node/ci/i18n-update-source.js
 tx config mapping-bulk --project scratch-addons-website --source-language en --type GITHUBMARKDOWN --file-extension .md --source-file-dir en/markdown --expression \<lang\>/markdown/{filepath}/{filename}{extension} --minimum-perc=0 --execute
-tx config mapping-bulk --project scratch-addons-website --source-language en --type HTML --file-extension .html --source-file-dir en/html-content --expression \<lang\>/html-content/{filepath}/{filename}{extension} --minimum-perc=0 --execute
+tx config mapping-bulk --project scratch-addons-website --source-language en --type HTML_FRAGMENT --file-extension .html --source-file-dir en/html-content --expression \<lang\>/html-content/{filepath}/{filename}{extension} --minimum-perc=0 --execute
 node ../script/node/ci/i18n-update-source-2.js
