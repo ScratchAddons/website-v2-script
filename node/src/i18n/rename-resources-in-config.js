88b3d76d7bea8d96b76892de76b731271cd2eda1
Hans5958 <email5958.1@gmail.com>
2021-05-20 10:27:18 +0700

    Fix typo

--- node/src/i18n/rename-resources-in-config.js
+++ node/src/i18n/rename-resources-in-config.js
@@ -6,7 +6,7 @@ module.exports = {
 	
 		configFile = configFile.replace(/\[scratch-addons-website.(.+)\]/g, (match, one) => {
 			if (one.startsWith("en_")) return match
-			return `[scractch-addons-website.en_${one}]`
+			return `[scratch-addons-website.en_${one}]`
 		})
 	
 		fs.writeFileSync(configFilePath, configFile)
@@ -15,7 +15,7 @@ module.exports = {
 		let configFile = fs.readFileSync(configFilePath, "utf-8")
 	
 		configFile = configFile.replace(/\[scratch-addons-website.en_(.+)\]/g, (match, one) => {
-			return `[scractch-addons-website.${one.replace("\\", "_")}]`
+			return `[scratch-addons-website.${one.replace("\\", "_")}]`
 		})
 	
 		fs.writeFileSync(configFilePath, configFile)
