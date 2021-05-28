90ef6162e93e41845dcf8bb34552b2c8640965e7
Hans5958 <email5958.1@gmail.com>
2021-05-28 23:29:28 +0700

    Add more pages to be translated

--- node/ci/consts.js
+++ node/ci/consts.js
@@ -1,7 +1,16 @@
 module.exports = {
 	gitEmail: "73682299+scratchaddons-bot[bot]@users.noreply.github.com",
 	gitName: "scratchaddons-bot[bot]",
-	contentGlobPatterns: ["_index.html", "welcome.html", "farewell.html", "addons.html", "docs/_index.md"],
+	contentGlobPatterns: [
+		"_index.html", 
+		"welcome.html", 
+		"farewell.html", 
+		"addons.html", 
+		"feedback.html", 
+		"scratch-messaging-transition.html", 
+		"unsupported-browser.html", 
+		"docs/_index.md"
+	],
 	translatableFrontMatterFields: ["title", "description"],
 	excludedFrontMatterFields: ["aliases"],
 	cleanUpGlobPatterns: ["!addons-data.json"]
