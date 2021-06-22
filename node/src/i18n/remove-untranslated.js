2045d33718fef8757969ab4c6a639b440fde7545
Hans5958 <email5958.1@gmail.com>
2021-06-22 11:15:05 +0700

    Remove untranslated strings for Hugo i18n strings
    Hugo itself will fallback to en if it is not found

--- node/src/i18n/remove-untranslated.js
+++ node/src/i18n/remove-untranslated.js
@@ -2,7 +2,22 @@ const fs = require("fs-extra")
 const globby = require("globby")
 const path = require("path")
 const chalk = require("chalk")
+const yaml = require("yaml")
 // const scmp = require("scmp")
+
+const removeSimilarEntries = (objectTarget, objectReference) => {
+
+	Object.keys(objectReference).forEach(keys => {
+		if (!objectTarget[keys]) return
+		if (typeof objectReference[keys] === "object") {
+			objectTarget[keys] = removeSimilarEntries(objectTarget[keys], objectReference[keys])
+			if (Object.keys(objectTarget[keys]).length === 0) delete objectTarget[keys]
+		}
+		else if (objectTarget[keys] === objectReference[keys]) delete objectTarget[keys]
+	})
+	return objectTarget
+
+}
 
 module.exports = (i18nLanguageDirPath, eni18nLanguageDirPath, options = {}) => {
 
@@ -20,18 +35,32 @@ module.exports = (i18nLanguageDirPath, eni18nLanguageDirPath, options = {}) => {
 
 		if (!fs.existsSync(i18nLanguageDirPath + filePath)) return
 
-		if (fs.readFileSync(eni18nLanguageDirPath + filePath, "utf-8").trim() === fs.readFileSync(i18nLanguageDirPath + filePath, "utf-8").trim()) {
-			console.log(chalk`{inverse ${i18nLanguageDirPath}${filePath}} is similar. Removing...`)
-			fs.removeSync(i18nLanguageDirPath + filePath)
-		} else {
-			// console.log(i18nLanguageDirPath + filePath + " is different")
-		}
+		if (filePath === "hugo-i18n.yml") {
 
-		// if (scmp(fs.readFileSync(eni18nLanguageDirPath + filePath), fs.readFileSync(i18nLanguageDirPath + filePath))) {
-		// 	console.log(filePath + " is similar")
-		// } else {
-		// 	console.log(filePath + " is different")
-		// }
+			let result = removeSimilarEntries(
+				yaml.parse(fs.readFileSync(i18nLanguageDirPath + "hugo-i18n.yml", "utf-8")),
+				yaml.parse(fs.readFileSync(eni18nLanguageDirPath + "hugo-i18n.yml", "utf-8"))
+			)
+
+			if (Object.keys(result).length) fs.writeFileSync(i18nLanguageDirPath + "hugo-i18n.yml", yaml.stringify(result))
+			else fs.writeFileSync(i18nLanguageDirPath + "hugo-i18n.yml", "")
+
+		} else {
+
+			if (fs.readFileSync(eni18nLanguageDirPath + filePath, "utf-8").trim() === fs.readFileSync(i18nLanguageDirPath + filePath, "utf-8").trim()) {
+				console.log(chalk`{inverse ${i18nLanguageDirPath}${filePath}} is similar. Removing...`)
+				fs.removeSync(i18nLanguageDirPath + filePath)
+			} else {
+				// console.log(i18nLanguageDirPath + filePath + " is different")
+			}
+	
+			// if (scmp(fs.readFileSync(eni18nLanguageDirPath + filePath), fs.readFileSync(i18nLanguageDirPath + filePath))) {
+			// 	console.log(filePath + " is similar")
+			// } else {
+			// 	console.log(filePath + " is different")
+			// }
+
+		}
 
 	})
 
