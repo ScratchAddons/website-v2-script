6051da92e9901bd3c895f9830c62fb896ba3226f
Hans5958 <email5958.1@gmail.com>
2021-06-23 21:21:06 +0700

    Extend translatable pages

--- node/ci/consts.js
+++ node/ci/consts.js
@@ -2,15 +2,14 @@ module.exports = {
 	gitEmail: "73682299+scratchaddons-bot[bot]@users.noreply.github.com",
 	gitName: "scratchaddons-bot[bot]",
 	contentGlobPatterns: [
-		"_index.html", 
-		"welcome.html", 
-		"farewell.html", 
-		"addons.html", 
-		"feedback.html", 
-		"scratch-messaging-transition.html", 
-		"unsupported-browser.html", 
-		"docs/_index.md"
-	],
+		"*",
+		"!contributor.html",
+		"docs/**",
+		"!docs/faq.md",        // Hold until it is deemed to be fine to be translated
+		"!docs/privacy/**",    // Decided not to translate
+		"!docs/policies/**",   // Same as above just in case it got moved
+		"!docs/reference/**",  // Hold it for later
+],
 	translatableFrontMatterFields: ["title", "description"],
 	excludedFrontMatterFields: ["aliases"],
 	cleanUpGlobPatterns: ["**", "!addons-data.json"],
