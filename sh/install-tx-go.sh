84f5ade6d3b95c825dbf2e0f936dc7161aab8c58
Hans5958 <hans5958@outlook.com>
2022-02-06 19:54:11 +0700

    Bugfix 4

--- sh/install-tx-go.sh
+++ sh/install-tx-go.sh
@@ -2,6 +2,6 @@ shopt -s expand_aliases
 source ~/.bashrc
 wget https://github.com/transifex/cli/releases/download/v1.0.0/tx-linux-amd64.tar.gz -O ${RUNNER_TEMP}/tx.tar.gz
 tar -zxvf ${RUNNER_TEMP}/tx.tar.gz -C ${RUNNER_TEMP} tx
-chmod -x ${RUNNER_TEMP}/tx'
+chmod -x '${RUNNER_TEMP}/tx'
 alias tx='${RUNNER_TEMP}/tx'
 echo "alias tx='${RUNNER_TEMP}/tx'" >> ~/.bash_aliases
