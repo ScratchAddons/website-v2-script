3813542aef1be5605001f3df347995ef01d22a34
Hans5958 <hans5958@outlook.com>
2022-02-06 19:46:37 +0700

    Final changes

--- sh/i18n-update-i18n.sh
+++ sh/i18n-update-i18n.sh
@@ -1,7 +1,5 @@
 npm install --prefix ../script
-# pip install transifex-client
 bash ../script/sh/install-tx-go.sh
 . ~/.bash_aliases
-# node ../script/node/ci/i18n-update-i18n.js
 tx pull --all --use-git-timestamps --skip --mode sourceastranslation
 node ../script/node/ci/i18n-update-i18n.js
--- sh/i18n-update-source.sh
+++ sh/i18n-update-source.sh
@@ -1,5 +1,4 @@
 npm install --prefix ../script
-# pip install transifex-client
 bash ../script/sh/install-tx-go.sh
 . ~/.bash_aliases
 node ../script/node/ci/i18n-update-source.js
