ec0ff6e3453a3aa6b93e70587feef2ae3fa15486
Hans5958 <hans5958@outlook.com>
2022-02-24 14:53:59 +0700

    Add workflow to update with force

---
+++ node/ci/i18n-update-i18n-force.js
@@ -0,0 +1,26 @@
+const { gitEmail, gitName } = require("./consts.js")
+const globby = require("globby")
+
+;(async () => {
+
+	globby.sync(["*", "!en"], {
+		onlyDirectories: true
+	}).forEach(langPath => {
+
+		require("../src/i18n/remove-untranslated.js")(
+			langPath + "/",
+			"en/"
+		)
+
+	})
+
+	// require("../src/i18n/generate-chinese-variants")("zh_TW/", "zh_CN/")
+	// require("../src/i18n/generate-chinese-variants")("zh_TW/", "zh_HK/")
+
+	await require("../src/git-commit-all-and-push.js")(
+		`Update localization files with force (${new Date().toISOString()})`,
+		gitEmail,
+		gitName
+	)
+
+})()
---
+++ sh/i18n-update-i18n-force.sh
@@ -0,0 +1,6 @@
+npm install --prefix ../script
+bash ../script/sh/install-tx-go.sh
+shopt -s expand_aliases 
+alias tx='${RUNNER_TEMP}/tx'
+tx pull --all --force --mode sourceastranslation
+node ../script/node/ci/i18n-update-i18n-force.js
