ed173e0db4d1a84d985f96a04fa01aecece7633e
Hans5958 <hans5958@outlook.com>
2022-03-02 08:47:28 +0700

    Attempt fixing removal of empty string 2

--- node/src/recursive-object-functions.js
+++ node/src/recursive-object-functions.js
@@ -1,13 +1,15 @@
 const removeSimilarEntries = (objectTarget, objectReference) => {
 
+	if (!objectTarget) return
+
 	Object.keys(objectReference).forEach(keys => {
-		if (!objectTarget || !objectTarget[keys]) return
-		if (typeof objectReference[keys] === "object") {
+		
+		if (!objectTarget[keys]) delete objectTarget[keys]
+		else if (typeof objectReference[keys] === "object") {
 			objectTarget[keys] = removeSimilarEntries(objectTarget[keys], objectReference[keys])
 			if (Object.keys(objectTarget[keys]).length === 0) delete objectTarget[keys]
 		}
 		else if (objectTarget[keys] === objectReference[keys]) delete objectTarget[keys]
-		else if (typeof objectTarget[keys] === "string" && objectTarget[keys] === "") delete objectTarget[keys]
 	})
 	return objectTarget
 
