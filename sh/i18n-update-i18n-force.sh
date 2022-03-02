9818f6f99f3cfa2d5ecf066d274d8df82230d40d
Hans5958 <hans5958@outlook.com>
2022-03-02 09:21:15 +0700

    Use install script from Transifex

--- sh/i18n-update-i18n-force.sh
+++ sh/i18n-update-i18n-force.sh
@@ -1,7 +1,4 @@
 npm install --prefix ../script
-bash ../script/sh/install-tx-go.sh
-shopt -s expand_aliases 
-alias tx='${RUNNER_TEMP}/tx'
 tx pull --all --force
 node ../script/node/ci/i18n-update-i18n-force.js
 node ../script/node/ci/i18n-update-i18n-force-2.js
--- sh/install-tx-go.sh
+++
@@ -1,2 +0,0 @@
-wget https://github.com/transifex/cli/releases/download/v1.0.2/tx-linux-amd64.tar.gz -O ${RUNNER_TEMP}/tx.tar.gz
-tar -zxvf ${RUNNER_TEMP}/tx.tar.gz -C ${RUNNER_TEMP} tx
