1074ae32b470177b959664275a1af974b0633930
Hans5958 <hans5958@outlook.com>
2022-03-02 12:01:17 +0700

    The script won't work! Use the old method

--- sh/i18n-update-i18n.sh
+++ sh/i18n-update-i18n.sh
@@ -1,3 +1,6 @@
 npm ci --prefix ../script
+bash ../script/sh/install-tx-go.sh
+shopt -s expand_aliases 
+alias tx='${RUNNER_TEMP}/tx'
 tx pull --all --use-git-timestamps --skip
 node ../script/node/ci/i18n-update-i18n.js
--- sh/i18n-update-source.sh
+++ sh/i18n-update-source.sh
@@ -1,2 +1,5 @@
 npm ci --prefix ../script
+bash ../script/sh/install-tx-go.sh
+shopt -s expand_aliases 
+alias tx='${RUNNER_TEMP}/tx'
 node ../script/node/ci/i18n-update-source.js
