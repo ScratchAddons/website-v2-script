c07bbc698b3affce282a5f892a08e5b35c774373
Hans5958 <hans5958@outlook.com>
2022-03-22 10:55:50 +0700

    Migrate from CJS to ESM 5

--- node/ci/consts.js
+++ node/ci/consts.js
@@ -1,19 +1,16 @@
-export default {
-	gitEmail: "73682299+scratchaddons-bot[bot]@users.noreply.github.com",
-	gitName: "scratchaddons-bot[bot]",
-	contentGlobPatterns: [
-		"*",
-		"!contributors.html",
-		"docs/**",
-		"!docs/policies/**",   // Decided not to translate
-		"!docs/reference/**",  // Hold it for later
-		"!blog/**"              // Decided not to translate (ScratchAddons/website-v2#159)
-],
-	translatableFrontMatterFields: ["title", "description"],
-	excludedFrontMatterFields: [],
-	cleanUpGlobPatterns: ["**", "!addons-data.json"],
-	txOrgSlug: "scratch-addons",
-	txProjectSlug: "scratch-addons-website",
-	txToken: process.env.TX_TOKEN,
-	// resourcesToPullWithAPI3: ["type:GITHUBMARKDOWN"]
-}
+export const gitEmail = "73682299+scratchaddons-bot[bot]@users.noreply.github.com";
+export const gitName = "scratchaddons-bot[bot]";
+export const contentGlobPatterns = [
+	"*",
+	"!contributors.html",
+	"docs/**",
+	"!docs/policies/**",
+	"!docs/reference/**",
+	"!blog/**" // Decided not to translate (ScratchAddons/website-v2#159)
+];
+export const translatableFrontMatterFields = ["title", "description"];
+export const excludedFrontMatterFields = [];
+export const cleanUpGlobPatterns = ["**", "!addons-data.json"];
+export const txOrgSlug = "scratch-addons";
+export const txProjectSlug = "scratch-addons-website";
+export const txToken = process.env.TX_TOKEN;
