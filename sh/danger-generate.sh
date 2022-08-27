8d49e8806399122865ffb1caff395ecdd973e764
Hans5958 <hans5958@outlook.com>
2022-08-27 13:19:40 +0700

    Install first?

--- sh/danger-generate.sh
+++ sh/danger-generate.sh
@@ -1,5 +1,7 @@
 cp -r ../script/danger/. ./
+npm ci --prefix ../script
 npx danger ci --text-only --verbose
+# node ../script/danger/generate.js
 rm -rf dangerfile.js
 rm -rf danger-src/
 echo $PR_NUMBER > ./pr-number
