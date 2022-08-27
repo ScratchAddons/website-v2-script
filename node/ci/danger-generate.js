971a5a460585af8e311f008abc32eb0524a61d36
Hans5958 <hans5958@outlook.com>
2022-08-28 00:34:54 +0700

    Expose to logs please

--- node/ci/danger-generate.js
+++ node/ci/danger-generate.js
@@ -9,12 +9,14 @@ import { context } from "@actions/github"
 const changedFiles = await getChangedFiles()
 
 // No PR is too small to include a description of why you made a change
+console.log(context.payload.pull_request.body, context.payload.pull_request.body.length, context.payload.pull_request.body.length > 10, !(context.payload.pull_request.body && context.payload.pull_request.body.length > 10))
 if (!(context.payload.pull_request.body && context.payload.pull_request.body.length > 10)) {
 	warn("Please include a description of your changes on the PR body.")
 }
 
 // Attention required on privacy policy edits
-if (matcher(changedFiles.changed, ['content/docs/policies/privacy/**']).length > 0) {
+console.log(changedFiles.changed, matcher(changedFiles.changed, ['content/docs/policies/privacy/**']), matcher(changedFiles.changed, ['content/docs/policies/privacy/**']).length)
+if (matcher(changedFiles.changed, ['content/docs/policies/privacy/**']).length) {
 	warn("Changes made on the privacy policy. Further review by @ WorldLanguages is needed.")
 }
 
