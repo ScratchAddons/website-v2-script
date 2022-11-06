1d109943bcba148b828fc768ffd8fcb16ec406ab
Hans5958 <hans5958@outlook.com>
2022-11-07 00:12:08 +0700

    Adjust for Giscus using js on i18n

--- node/src/i18n/get-translated-giscus.js
+++ node/src/i18n/get-translated-giscus.js
@@ -4,7 +4,9 @@ import axios from "axios"
 
 export default async (jsonPath, options = {}) => {
 
-	const giscusi18n = await axios.get('https://raw.githubusercontent.com/giscus/giscus/main/i18n.json').then(response => response.data)
+	const giscusi18n = await axios.get('https://raw.githubusercontent.com/giscus/giscus/main/i18n.json')
+		.then(response => response.data)
+		.then(data => JSON.parse(`{ "locales": ${data.match(/locales:\s*(\[.+?])/s)[1].replaceAll('\'', '"').replace(/,\s*?]/s, ']')}}`))
 
 	console.log(chalk`Adding Giscus languages...`)
 	fs.outputJSONSync(jsonPath, giscusi18n.locales)
