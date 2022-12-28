12e8f954c2451d303fc5d84cd2e2813ff6fa3fa7
Hans5958 <hans5958@outlook.com>
2022-12-28 18:38:24 +0700

    Update security.txt to be always 28 days since deployment

--- sh/hugo-deploy-site.sh
+++ sh/hugo-deploy-site.sh
@@ -1,3 +1,9 @@
+# Prepare contributors
 wget https://raw.githubusercontent.com/ScratchAddons/contributors/with-commits/contributors.json -O "./data/credits/contributors.json"
+
+# Prepare security.txt expirity date (28 days later)
+sed -i "s/Expires: .*/Expires: $(date +"%Y-%m-%dT%H:%M:%S%z" -d "4 week")/g" static/.well-known/security.txt
+
+# Prepare translations
 npm ci --prefix ../script
 node ../script/node/ci/hugo-add-i18n.js
