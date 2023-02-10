7b514acbeed5c94ae7d9147dc3cbb9f33a1fe17b
Hans5958 <hans5958@outlook.com>
2023-02-10 23:54:05 +0700

    Update dependencies

--- sh/install-tx-go.sh
+++ sh/install-tx-go.sh
@@ -1,2 +1,2 @@
-wget https://github.com/transifex/cli/releases/download/v1.0.2/tx-linux-amd64.tar.gz -O ${RUNNER_TEMP}/tx.tar.gz
+wget https://github.com/transifex/cli/releases/download/v1.6.4/tx-linux-amd64.tar.gz -O ${RUNNER_TEMP}/tx.tar.gz
 tar -zxvf ${RUNNER_TEMP}/tx.tar.gz -C ${RUNNER_TEMP} tx
