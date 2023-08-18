4bc31a6e01872e77240449c34da36af1cfd81e3b
Hans5958 <hans5958@outlook.com>
2023-08-18 14:09:11 +0700

    Update Transifex client to v1.6.10

--- sh/install-tx-go.sh
+++ sh/install-tx-go.sh
@@ -1,2 +1,2 @@
-wget https://github.com/transifex/cli/releases/download/v1.6.7/tx-linux-amd64.tar.gz -O ${RUNNER_TEMP}/tx.tar.gz
+wget https://github.com/transifex/cli/releases/download/v1.6.10/tx-linux-amd64.tar.gz -O ${RUNNER_TEMP}/tx.tar.gz
 tar -zxvf ${RUNNER_TEMP}/tx.tar.gz -C ${RUNNER_TEMP} tx
