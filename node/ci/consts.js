d48371a95d87850ea4879fcdac139b3db4fe2c04
Hans5958 <hans5958@outlook.com>
2024-06-23 16:34:33 +0700

    Expand translatable content

--- node/ci/consts.js
+++ node/ci/consts.js
@@ -1,11 +1,11 @@
 export const gitEmail = "73682299+scratchaddons-bot[bot]@users.noreply.github.com";
 export const gitName = "scratchaddons-bot[bot]";
 export const contentGlobPatterns = [
-	"*",
-	"docs/**",
+	"**",
 	"!docs/policies/**",    // Decided not to translate
 	"!docs/reference/**",   // Hold it for later
-	"!blog/**"              // Decided not to translate (ScratchAddons/website-v2#159)
+	"!blog/**",             // Decided not to translate (ScratchAddons/website-v2#159)
+	"!redirect/**"          // Redirects are NOT translatable
 ];
 export const pathTranslationExceptionRegexPatterns = [
 	"^/docs/policies.*",
